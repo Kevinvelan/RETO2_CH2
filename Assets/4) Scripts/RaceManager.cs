9603b54f3326eef943c06f0bbe3abb9dac11c991
Kevinvelan <kevin.velandia.tecni@gmail.com>
2023-12-17 01:04:32 -0500

    correccion de chekpoints
    kevin 17/12/2023

--- Assets/4) Scripts/RaceManager.cs
+++ Assets/4) Scripts/RaceManager.cs
@@ -38,6 +38,8 @@ public class RaceManager : MonoBehaviour
         for (int i = 0; i < allCheckpoints.Length; i++)
         {
             allCheckpoints[i].cpNumber = i;
+
+            Debug.Log(i);
         }
 
         isStarting = true;
